d62fefa9603ec488c8b87cd5257e1605dbc7eb72
Vov4ik3587 <galstyan.vova@gmail.com>
2022-05-19 03:05:36 +0700

    НЕ работает

---
+++ URVS-RGZ/my_library.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "framework.h"
+#include <stdio.h>
+
+extern "C" __declspec(dllexport) int Information(char* InfoString)
+{
+	int keybord_type = GetKeyboardType(0); // Функция получения информации о типе клавиатуры
+	switch (keybord_type)
+	{
+	case 0x4:
+		strcpy(InfoString, "This is enhanced keyboard which has 101 or 102 keys and Numpad");
+		break;
+	case 0x7:
+		strcpy(InfoString, "This is Japanese keybord");
+		break;
+	case 0x8:
+		strcpy(InfoString, "This is Korean keyboard");
+		break;
+	case 0x51:
+		strcpy(InfoString, "Unknown type or HID keyboard");
+		break;
+	default:
+		strcpy(InfoString, "Something's gone wrong");
+		break;
+	}
+
+	return 0;
+}
